dce360285be2332fc50971d7136e85b581519259
22pop.fm <panpakapan0326@gmail.com>
2021-06-25 02:03:28 +0900

    [add] gemfile.

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,6 +1,35 @@
-require "json"
-require "aws-sdk-dynamodb"
+require 'json'
+require 'aws-sdk-dynamodb'
 
-def execute(event:, context:)    
-  return { execute_status: "ok", context: event }
+TABLE_NAME = "tyda-event"
+
+def execute(event:, context:)
+  Dotenv.load
+  event["events"].each { |e| put_event(e) }
+  { execute_status: "ok" }
+end
+
+private
+
+def put_event(e)
+  e.store("created_at", Time.now.to_s)
+  item = {
+    table_name: TABLE_NAME,
+    item: e,
+  }
+  dynamo_client.put_item(item)
+end
+
+def dynamo_client
+  Aws::DynamoDB::Client.new(
+    region: ENV['REGION'],
+    credentials: credentials,    
+  )
+end
+
+def credentials
+  Aws::Credentials.new(
+    ENV['ACCESS_KEY_ID'],
+    ENV['SECRET_ACCESS_KEY'],
+  )
 end
